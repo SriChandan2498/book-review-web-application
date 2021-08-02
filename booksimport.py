04bd86f2deb2e3f8e4e17df5b3c5bde27495f0cc
Sri Sai Chandan Jinka <71498458+SriChandan2498@users.noreply.github.com>
2021-08-02 12:42:20 +0530

    Update booksimport.py

--- booksimport.py
+++ booksimport.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from models import BookDetails
 
-engine = create_engine('postgresql://gkuevkgkttisvm:3837757d6a34f1762ceafc9a2d3ee7af6aa2776c45890bd21c8be9c7238d3082@ec2-52-45-183-77.compute-1.amazonaws.com:5432/d397aj7gdd13fg')
+engine = create_engine(os.getenv('HEROKU_DB_URL'))
 db_session = scoped_session(sessionmaker(bind=engine))
 with open('books.csv') as csvfile:
     records = list(csv.reader(csvfile,delimiter=','))
